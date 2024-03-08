4ce667781df336d53c02851013375ec4fec91bc6
amileshin <amileshin@list.ru>
2024-03-08 18:11:03 +0300

    Creation using spring initializr and add readme.md

---
+++ src/main/java/com/teaching/masteragent/MasterAgentApplication.java
@@ -0,0 +1,13 @@
+package com.teaching.masteragent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teaching/masteragent/MasterAgentApplicationTests.java
@@ -0,0 +1,6 @@
+package com.teaching.masteragent;
+
+import org.junit.jupiter.api.Test;
+
+class MasterAgentApplicationTests {
+}
